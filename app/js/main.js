a1ca91fe13b4f4be9ae12828aedb946da8b3369f
Кирилл Зуев <kirillzuev@ip-192-168-0-101.is74.loc>
2020-10-26 22:32:09 +0500

    add popup form

--- app/js/main.js
+++ app/js/main.js
@@ -40,3 +40,23 @@
          header.classList.remove("sticky");
      }
  }
+//var popupSmWidth = $( ".popup_sm" ).height();
+$('.btn:contains(Let’s Talk)').click(function() {
+    $(".popup_body_sm").addClass("popupactiv");
+    //$( "#body" ).css("position" , "fixed");
+    //$( "#body" ).css("overflow-y" , "scroll");
+    //$( "#body" ).css("width" , "100%");
+    //$( "#body" ).css("overflow", "hidden");
+    //$( ".pages__wrap" ).css("overflow-y", "scroll");
+    $(".bodyW").toggleClass("wbody__active");
+})
+$(".popup_body_sm__close").click(function() {
+
+    //$( "#body" ).css("position" , "static");
+    //$( "#body" ).css("overflow-y" , "scroll");
+    //$( "#body" ).css("width" , "100%");
+    $(".popup_body_sm").removeClass("popupactiv");
+    //$( "#body" ).css("overflow" , "auto");
+    $(".bodyW").removeClass("wbody__active");
+    //$( ".popup_body_sm" ).css("height" , "auto");
+})
