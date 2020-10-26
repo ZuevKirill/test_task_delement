570aec6160a50937ea2efd07f6e05dad3f6bc191
Кирилл Зуев <kirillzuev@ip-192-168-0-101.is74.loc>
2020-10-26 22:58:42 +0500

    add send form

--- app/js/main.js
+++ app/js/main.js
@@ -40,23 +40,43 @@
          header.classList.remove("sticky");
      }
  }
-//var popupSmWidth = $( ".popup_sm" ).height();
 $('.btn:contains(Let’s Talk)').click(function() {
     $(".popup_body_sm").addClass("popupactiv");
-    //$( "#body" ).css("position" , "fixed");
-    //$( "#body" ).css("overflow-y" , "scroll");
-    //$( "#body" ).css("width" , "100%");
-    //$( "#body" ).css("overflow", "hidden");
-    //$( ".pages__wrap" ).css("overflow-y", "scroll");
+
     $(".bodyW").toggleClass("wbody__active");
 })
 $(".popup_body_sm__close").click(function() {
 
-    //$( "#body" ).css("position" , "static");
-    //$( "#body" ).css("overflow-y" , "scroll");
-    //$( "#body" ).css("width" , "100%");
     $(".popup_body_sm").removeClass("popupactiv");
-    //$( "#body" ).css("overflow" , "auto");
+
     $(".bodyW").removeClass("wbody__active");
-    //$( ".popup_body_sm" ).css("height" , "auto");
 })
+
+
+jQuery(document).ready(function($) {
+    $(".form").submit(function() {
+        var str = $(this).serialize();
+        console.log(str);
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: str,
+            success: function(msg) {
+                if (msg == 'Отправленно') {
+                    result = msg;
+                    //location.href = '';
+                } else {
+                    result = msg;
+
+                }
+
+                //$('.submitb').text(result);
+                $('.form').css("display", "none");
+                $('.popup_sm__h1').text("You mes send!");
+                $('.submitb').css("display", "none");
+
+            }
+        });
+        return false;
+    });
+});
